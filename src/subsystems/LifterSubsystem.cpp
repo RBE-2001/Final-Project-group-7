9e1216fced16afa331513823a6f4cc0b1f699da4
Sam Duckett <samjduckett@gmail.com>
2025-10-06 00:22:06 -0400

    adding lifter code

---
+++ src/subsystems/LifterSubsystem.cpp
@@ -0,0 +1,104 @@
+#include "LifterSubsystem.h"
+
+// --------- Susytems Functions ---------
+void LifterSubsystem::Init() {
+    currentState = State::Idle;
+
+#ifdef __SUBSYSTEM_DEBUG
+    Serial.println("LifterSubsystem -> Initilized");
+#endif
+}
+
+void LifterSubsystem::Update() {
+    switch (currentState) {
+        case State::Idle:
+            Idle();
+            break;
+
+        case State::L1:
+            GoToL(1);
+            break;
+
+        case State::L15:
+            GoToL(1.5);
+            break;
+
+        case State::L2:
+            GoToL(2);
+            break;
+
+        case State::L25:
+            GoToL(2.5);
+            break;
+
+        case State::L3:
+            GoToL(3);
+            break;
+    }
+}
+
+bool LifterSubsystem::IsDone() const {
+    return currentState == State::Idle;
+}
+
+// --------- States ---------
+void LifterSubsystem::Idle() {
+    //TODO: stop motors
+}
+
+void LifterSubsystem::GoToL(float rot) {
+    // TODO: replace with set Lifter to L code
+
+    if (true) { //TODO: replace true with logic for when Lifter is at Leval
+        SetState(State::Idle);  // go back to idle when done
+    }
+}
+
+void LifterSubsystem::SetState(State newState) {
+    if (newState != currentState) {
+        previousState = currentState;
+        currentState = newState;
+
+    #ifdef __SUBSYSTEM_DEBUG
+        switch (newState) {
+            case State::Idle:       Serial.println("LifterSubsystem -> Entering IDLE"); break;
+            case State::L1:  Serial.println("LifterSubsystem -> Goint to L1"); break;
+            case State::L15:  Serial.println("LifterSubsystem -> Goint to L1.5"); break;
+            case State::L2:  Serial.println("LifterSubsystem -> Goint to L2"); break;
+            case State::L25:  Serial.println("LifterSubsystem -> Goint to L2.5"); break;
+            case State::L3:  Serial.println("LifterSubsystem -> Goint to L3"); break;
+        }
+    #endif
+    }
+}
+
+// --------- Commands ---------
+void LifterSubsystem::L1Command() {
+    if (currentState == State::Idle) {
+        SetState(State::L1);
+    }
+}
+
+void LifterSubsystem::L15Command() {
+    if (currentState == State::Idle) {
+        SetState(State::L15);
+    }
+}
+
+void LifterSubsystem::L2Command() {
+    if (currentState == State::Idle) {
+        SetState(State::L2);
+    }
+}
+
+void LifterSubsystem::L25Command() {
+    if (currentState == State::Idle) {
+        SetState(State::L25);
+    }
+}
+
+void LifterSubsystem::L3Command() {
+    if (currentState == State::Idle) {
+        SetState(State::L3);
+    }
+}
---
+++ src/subsystems/LifterSubsystem.h
@@ -0,0 +1,32 @@
+#pragma once
+#include "Subsystem.h"
+
+class LifterSubsystem : public Subsystem {
+public:
+    void Init() override;
+    void Update() override;
+    bool IsDone() const override;
+
+    void L1Command();
+    void L15Command();
+    void L2Command();
+    void L25Command();
+    void L3Command();
+
+private:
+    enum class State {
+        Idle,
+        L1,
+        L15,
+        L2,
+        L25,
+        L3,
+    };
+
+    State currentState = State::Idle;
+    State previousState = State::Idle;
+
+    void SetState(State newState);
+    void Idle();
+    void GoToL(float rot);
+};
